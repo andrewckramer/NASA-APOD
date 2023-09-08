17991253f571319fafebc6f8a04c6acb599da48d
Andrew Kramer <144305755+andrewckramer@users.noreply.github.com>
2023-09-07 18:01:11 -0700

    Update nasa_apod.py
    updated nasa_apod.py to work with the updated file organization, now calls the function apod_gui and passes it the parameters text and image to create the gui using the provided text and image

--- code/nasa_apod.py
+++ code/nasa_apod.py
@@ -11,6 +11,7 @@ import requests
 import os
 from PIL import Image, ImageTk
 import urllib.request
+from apod_gui import apod_gui
 from dotenv import load_dotenv
 from save_image import save_image
 import tkinter as tk
@@ -53,38 +54,10 @@ def main():
     max_size = (400, 800)
     img.thumbnail(max_size)
     
+    # Passes the image and text to the gui
     text = f'Explanation: \n\n{explanation_text}\n\nURL: \n{url_text}'
-    
-    # Creates the GUI
-    parent = tk.Tk()
-    parent.geometry("700x875")
-    parent.title("NASA APOD")
+    apod_gui(text, img)
 
-    image_frame = tk.Frame(parent, height=400, width=800)
-    image_frame.pack(side=tk.TOP)
-    
-    # Displays the image inside the gui
-    photo = ImageTk.PhotoImage(img)
-    label = tk.Label(image_frame, image = photo, height=400, width=800)
-    label.pack(expand = tk.YES)
-    
-    # Writes the explanation and URL to a text box in the GUI
-    text_box = tk.Text(parent)
-    text_box.pack()
-    text_box.insert(tk.END, text)
-    text_box.config(state=tk.DISABLED)
-    
-    # Labels and buttons for saving the image if the user chooses
-    label_text = tk.Label(text='Do you want to save the image?', height=2, font=(12))
-    label_text.pack()
-    
-    yes_button = tk.Button(text='Yes', height=2, command = lambda : save_image(image_url, parent))
-    yes_button.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
-    no_button = tk.Button(text='No', height=2, command = lambda : parent.destroy())
-    no_button.pack(side=tk.RIGHT, expand=True, fill=tk.BOTH)
-    
-    parent.mainloop()
-    
     img.close()
     
 if __name__ == '__main__':
