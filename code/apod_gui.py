27a0667e1e29126abd5a1801fec1cc41a47bb29a
Andrew Kramer <144305755+andrewckramer@users.noreply.github.com>
2023-09-20 12:38:10 -0700

    Update apod_gui.py
    resize the image frame based on the size of the image for a more appealing look

--- code/apod_gui.py
+++ code/apod_gui.py
@@ -19,15 +19,18 @@ def apod_gui(text, img, image_url, copyright):
     parent.title('APOD Test')
     parent.geometry('1250x750')
 
-
-    # Creates the frame for the image and displays the image
-    image_frame = ctk.CTkFrame(parent, height=750, width=800)
-    image_frame.pack(side='left')
-
-    info_frame = ctk.CTkFrame(parent)
-    info_frame.pack(side='right', anchor='n')
+    width_base = 1250
 
     width, height = img.size
+
+    
+    # Creates the frame for the image and displays the image
+    image_frame = ctk.CTkFrame(parent, height=750, width=width)
+    image_frame.pack(side='left')
+
+    info_frame = ctk.CTkFrame(parent, height=750, width=width_base-width)
+    info_frame.pack(side='right', anchor='n')
+
 
     image = ctk.CTkImage(dark_image=img, size=(width, height))
 
@@ -38,7 +41,7 @@ def apod_gui(text, img, image_url, copyright):
     # If there is copyright information, displays it below the image
     if copyright != None:
         copyright_text = f"\u00A9 {copyright}"
-        copyright_label = ctk.CTkLabel(image_frame, height=25, width=800, font=('Segoe UI', 10), text=copyright_text)
+        copyright_label = ctk.CTkLabel(image_frame, height=25, width=width, font=('Segoe UI', 10), text=copyright_text)
         copyright_label.pack(side='bottom', padx=5)
 
 
@@ -47,23 +50,23 @@ def apod_gui(text, img, image_url, copyright):
     logo_path = os.path.join(cur_path, '..', 'assets', 'resized_apod_logo.png')
     logo_img = Image.open(logo_path)
 
-    width, height = logo_img.size
+    logo_width, logo_height = logo_img.size
 
-    logo_image = ctk.CTkImage(dark_image=logo_img, size=(width, height))
+    logo_image = ctk.CTkImage(dark_image=logo_img, size=(logo_width, logo_height))
 
-    title_label = ctk.CTkLabel(info_frame, text='', height=100, width=450, image=logo_image)
+    title_label = ctk.CTkLabel(info_frame, text='', height=100, width=logo_width, image=logo_image)
     title_label.grid(row=0, columnspan=2) 
 
 
     # Creates the textbox to display the title, description, and URL
-    textbox = ctk.CTkTextbox(info_frame, width=400, height=500, wrap='word', font=('Segoe UI', 14))
+    textbox = ctk.CTkTextbox(info_frame, width=width_base-width, height=500, wrap='word', font=('Segoe UI', 14))
     textbox.grid(row=1, columnspan=2)
     textbox.insert('0.0', text)
     textbox.configure(state='disabled')
 
 
     # Creates the save button to save the image
-    save_button = ctk.CTkButton(info_frame, text='Save Image', width=300, height=50, command = lambda : save_image(image_url, parent))
+    save_button = ctk.CTkButton(info_frame, text='Save Image', width=width_base-width, height=50, command = lambda : save_image(image_url, parent))
     save_button.grid(row=2, columnspan=2)
 
 
